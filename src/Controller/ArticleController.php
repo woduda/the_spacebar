485d0c28a70cf323f79a61df4467244aa303f05f
Wojciech Duda <wo.duda@gmail.com>
2021-01-29 22:47:10 +0100

    Routes

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController 
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Welcome there!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response("I'm $slug");
+    }
+
+}
